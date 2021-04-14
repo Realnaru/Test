29c202505353460cb3ae4fa0fd593cf3d5ca1122
Ruslan Opishniak <73569436+Realnaru@users.noreply.github.com>
2021-04-14 08:59:52 +0800

    Update app.js
    Add coments

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
+//require express, body-parser, pg(to work with postgreSQL)
 const express = require("express");
 const bodyParser = require("body-parser");
 const pg = require("pg");
-let isDataBaseCreated = false;
-let isTableCreated = false;
+
+let isDataBaseCreated = false;//to check if Data Base already created
+let isTableCreated = false;//to check if table already created
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("public"));//use folder "public" to store .css and .js files
 
+//if Data Base and table is not created yet then create
 if (dataBaseCreated === false && tableCreated === false) {
   createDataBase();
   createTable();
 }
 
+//handling get request to the "/" route
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");//send index.html
 });
 
+//handling POST request from index.html
 app.post(process.env.port || "/", (req, res) => {
-  let sortIdent = req.body.identifierInput;
-  let sortedArray = req.body.arrayOutput.split(",");
-  console.log(sortIdent, sortedArray);
+  let sortIdent = req.body.identifierInput;//get identifier 
+  let sortedArray = req.body.arrayOutput.split(",");//sorted array as string, return an array and store the array 
+  console.log(sortIdent, sortedArray);//log variables to ensure that all works well
 
+  //insert pares of elemnts of the array and identifier into data base table 
   for (let i = 0; i < sortedArray.length; i++) {
     inserData(+sortedArray[i], sortIdent);
   }
-  res.sendFile(__dirname + "/fetch.html");
+  res.sendFile(__dirname + "/fetch.html");//send fetch.html
 
 });
 
+//handling POST request from fetch.html
 app.post("/fetch", (req, res) => {
   
-  let result = [];
-  let identifier = req.body.identifierInput;
+  let result = [];//new empty array to store results of the query
+  let identifier = req.body.identifierInput;//get identifier value
+  
+  //new connect to the Data Base
   const pool = new pg.Pool({
       user: 'postgres',
       host: '127.0.0.1',
@@ -42,20 +51,23 @@ app.post("/fetch", (req, res) => {
       port: '5432'}
   );
 
+  //select values from the table with needed identifier
   pool.query("select value from values where identifier = '" + identifier +"' order by value;",
       (err, results) => {
+      //if there is an error, show it
       if (err) {
         console.log(err);
       }
       console.log(results);
-      result = results.rows;
+      result = results.rows;//store selected values
       pool.end();
   });
 
   res.write("<h1>Here is your sorting</h1>");
-  res.send(result);
+  res.send(result);//show selectes values
 })
 
+//create new Data Base
 function createDataBase() {
   const pool = new pg.Pool({
       user: 'postgres',
@@ -72,6 +84,7 @@ function createDataBase() {
   isDataBaseCreated = true;
 }
 
+//create new table
 function createTable() {
   const pool = new pg.Pool({
       user: 'postgres',
@@ -88,6 +101,7 @@ function createTable() {
   isTableCreated = true;
 }
 
+//insert data to created database
 function inserData(value1, value2) {
   const pool = new pg.Pool({
       user: 'postgres',
@@ -103,6 +117,7 @@ function inserData(value1, value2) {
 })
 }
 
+//running the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
