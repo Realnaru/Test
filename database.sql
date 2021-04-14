cfcc15f47eebad82976091e4f2394b2573ea0d3a
Ruslan Opishniak <73569436+Realnaru@users.noreply.github.com>
2021-04-14 08:34:28 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
 
 CREATE DATABASE sortings;
 
-CREATE TABLE values(Id serial primary key, ArrayValue integer, identifier character varying(255));
+CREATE TABLE values(Id serial primary key, value integer, identifier character varying(255));
