eb7968ff52466b67b91f259a8c9fd15f325f98be
Ruslan Opishniak <73569436+Realnaru@users.noreply.github.com>
2021-04-15 09:12:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ function getValues(arrayOfObj) {
   return result;
 }
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
